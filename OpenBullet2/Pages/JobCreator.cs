e43a9a94b137d210be7aff53d9a268fb0c7664da
ruri <ruridev@protonmail.com>
2020-05-23 05:11:40 +0200

    Job creator and single run options GUI

---
+++ OpenBullet2/Pages/JobCreator.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using OpenBullet2.Entities;
+using OpenBullet2.Models.Jobs;
+using OpenBullet2.Repositories;
+using OpenBullet2.Services;
+using RuriLib.Models.Jobs;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenBullet2.Pages
+{
+    public partial class JobCreator
+    {
+        [Inject] IJobRepository JobRepo { get; set; }
+        [Inject] JobManagerService Manager { get; set; }
+        [Parameter] public string Type { get; set; }
+
+        Job job;
+
+        protected override void OnInitialized()
+        {
+            Type ??= JobType.MultiRun.ToString();
+
+            job = Enum.Parse(typeof(JobType), Type) switch
+            {
+                JobType.SingleRun => new SingleRunJob(),
+                JobType.MultiRun => new MultiRunJob(),
+                JobType.Spider => new SpiderJob(),
+                JobType.Ripper => new RipJob(),
+                JobType.SeleniumUnitTest => new SeleniumUnitTestJob(),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        private async Task Create()
+        {
+            var factory = new JobOptionsFactory();
+            var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
+            var wrapper = new JobOptionsWrapper { Options = factory.GetOptions(job) };
+
+            var entity = new JobEntity
+            {
+                CreationDate = DateTime.Now,
+                JobType = GetJobType(job),
+                JobOptions = JsonConvert.SerializeObject(wrapper, settings)
+            };
+
+            await JobRepo.Add(entity);
+
+            // Get the entity that was just added in order to get its ID
+            entity = await JobRepo.GetAll().OrderByDescending(e => e.Id).FirstAsync();
+
+            job.Id = entity.Id;
+            Manager.Jobs.Add(job);
+        }
+
+        private JobType GetJobType(Job job)
+        {
+            return job switch
+            {
+                SingleRunJob _ => JobType.SingleRun,
+                MultiRunJob _ => JobType.MultiRun,
+                RipJob _ => JobType.Ripper,
+                SpiderJob _ => JobType.Spider,
+                SeleniumUnitTestJob _ => JobType.SeleniumUnitTest,
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
--- OpenBullet2/Pages/JobManager.cs
+++ OpenBullet2/Pages/JobManager.cs
@@ -1,15 +1,11 @@
 ﻿using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using OpenBullet2.Entities;
 using OpenBullet2.Models.Jobs;
 using OpenBullet2.Repositories;
 using OpenBullet2.Services;
 using OpenBullet2.Shared.Forms;
 using RuriLib.Models.Jobs;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace OpenBullet2.Pages
@@ -36,54 +32,9 @@ namespace OpenBullet2.Pages
             {
                 var type = result.Data as JobType?;
 
-                Job job = type switch
-                {
-                    JobType.SingleRun => new SingleRunJob(),
-                    JobType.MultiRun => new MultiRunJob(),
-                    JobType.Spider => new SpiderJob(),
-                    JobType.Ripper => new RipJob(),
-                    JobType.SeleniumUnitTest => new SeleniumUnitTestJob(),
-                    _ => throw new NotImplementedException()
-                };
 
-                await Add(job);
-                Nav.NavigateTo($"job/create/{job.Id}");
+                Nav.NavigateTo($"jobs/create/{type}");
             }
-        }
-
-        private async Task Add(Job job)
-        {
-            var factory = new JobOptionsFactory();
-            var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
-            var wrapper = new JobOptionsWrapper { Options = factory.GetOptions(job) };
-
-            var entity = new JobEntity
-            {
-                CreationDate = DateTime.Now,
-                JobType = GetJobType(job),
-                JobOptions = JsonConvert.SerializeObject(wrapper, settings)
-            };
-
-            await JobRepo.Add(entity);
-
-            // Get the entity that was just added in order to get its ID
-            entity = await JobRepo.GetAll().OrderByDescending(e => e.Id).FirstAsync();
-
-            job.Id = entity.Id;
-            Manager.Jobs.Add(job);
-        }
-
-        private JobType GetJobType(Job job)
-        {
-            return job switch
-            {
-                SingleRunJob _ => JobType.SingleRun,
-                MultiRunJob _ => JobType.MultiRun,
-                RipJob _ => JobType.Ripper,
-                SpiderJob _ => JobType.Spider,
-                SeleniumUnitTestJob _ => JobType.SeleniumUnitTest,
-                _ => throw new NotImplementedException()
-            };
         }
 
         public async Task Remove(Job job)
