f94ef5905056977145a9db0860c2fa77453e5488
Ruri <ruridev@protonmail.com>
2020-10-07 03:24:00 +0200

    Removed a function that is now in RuriLib

--- OpenBullet2/Pages/Index.cs
+++ OpenBullet2/Pages/Index.cs
@@ -46,17 +46,6 @@ namespace OpenBullet2.Pages
             }), null, 500, 500);
         }
 
-        private string BytesToString(long byteCount)
-        {
-            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
-            if (byteCount == 0)
-                return "0" + $" {suf[0]}";
-            long bytes = Math.Abs(byteCount);
-            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
-            return (Math.Sign(byteCount) * num).ToString("0.##", CultureInfo.InvariantCulture) + $" {suf[place]}";
-        }
-
         private async Task Logout()
         {
             await ((Auth.OBAuthenticationStateProvider)Auth).Logout();
