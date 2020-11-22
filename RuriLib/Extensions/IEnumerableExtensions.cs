9208e4d33135429b8b2fc795a2e3db65fd0bef5d
Ruri <ruridev@protonmail.com>
2020-11-22 02:05:19 +0100

    Fixed error in InputDictionaryOfStrings for entries with same key

--- RuriLib/Extensions/IEnumerableExtensions.cs
+++ RuriLib/Extensions/IEnumerableExtensions.cs
@@ -9,5 +9,18 @@ namespace RuriLib.Extensions
     {
         public static IEnumerable<T> Clone<T>(this IEnumerable<T> enumerable)
             => enumerable.Select(item => JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(item)));
+
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
     }
 }
