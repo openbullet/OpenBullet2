0d38f1bc0f603f94a555bab9b920c0bfaa50a83b
Ruri <ruridev@protonmail.com>
2020-12-29 01:29:40 +0100

    Test with Grid.Blazor by gustavnavar

--- OpenBullet2/Pages/Hits.cs
+++ OpenBullet2/Pages/Hits.cs
@@ -1,14 +1,22 @@
 ﻿using Blazored.Modal;
 using Blazored.Modal.Services;
+using GridBlazor;
+using GridBlazor.Pages;
+using GridMvc.Server;
+using GridShared;
+using GridShared.Utility;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
 using OpenBullet2.Auth;
 using OpenBullet2.Entities;
 using OpenBullet2.Helpers;
 using OpenBullet2.Repositories;
 using OpenBullet2.Shared.Forms;
 using Radzen.Blazor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,6 +35,10 @@ namespace OpenBullet2.Pages
 
         RadzenGrid<HitEntity> hitsGrid;
         private int resultsPerPage = 15;
+
+        private GridComponent<HitEntity> _gridComponent;
+        private CGrid<HitEntity> _grid;
+        private Task _task;
 
         private string CaptureWidth
         {
@@ -54,10 +66,38 @@ namespace OpenBullet2.Pages
 
         protected override async Task OnInitializedAsync()
         {
-            uid = await ((OBAuthenticationStateProvider)Auth).GetCurrentUserId();
-            await RefreshList();
+            
+        }
 
-            await base.OnInitializedAsync();
+        protected override async Task OnParametersSetAsync()
+        {
+            uid = await ((OBAuthenticationStateProvider)Auth).GetCurrentUserId();
+
+            hits = uid == 0
+                ? await HitRepo.GetAll().ToListAsync()
+                : await HitRepo.GetAll().Where(h => h.OwnerId == uid).ToListAsync();
+
+            Action<IGridColumnCollection<HitEntity>> columns = c =>
+            {
+                c.Add(h => h.Data).Sortable(true).Filterable(true);
+                c.Add(h => h.Type).Sortable(true).Filterable(true);
+                c.Add(h => h.ConfigName).Sortable(true).Filterable(true);
+                c.Add(h => h.Date).Sortable(true).Filterable(true);
+                c.Add(h => h.WordlistName).Sortable(true).Filterable(true);
+                c.Add(h => h.Proxy).Sortable(true).Filterable(true);
+                c.Add(h => h.CapturedData).Sortable(true).Filterable(true);
+            };
+
+            var query = new QueryDictionary<StringValues>();
+            query.Add("grid-page", "2");
+
+            var client = new GridClient<HitEntity>(q => GetGridRows(columns, q), query, false, "hitsGrid", columns)
+                .Selectable(true, true, true);
+            _grid = client.Grid;
+
+            // Set new items to grid
+            _task = client.UpdateGrid();
+            await _task;
         }
 
         private async Task OnResultsPerPageChanged(int value)
@@ -71,12 +111,21 @@ namespace OpenBullet2.Pages
             selectedHit = hit;
         }
 
+        protected void OnHitSelected(object item)
+        {
+            if (item.GetType() == typeof(HitEntity))
+            {
+                selectedHit = (HitEntity)item;
+            }
+        }
+
         private async Task RefreshList()
         {
             hits = uid == 0 
                 ? await HitRepo.GetAll().ToListAsync()
                 : await HitRepo.GetAll().Where(h => h.OwnerId == uid).ToListAsync();
 
+            await _gridComponent.UpdateGrid();
             StateHasChanged();
         }
 
@@ -136,5 +185,15 @@ namespace OpenBullet2.Pages
 
         private async Task ShowNoHitSelectedWarning()
             => await js.AlertError(Loc["Uh-Oh"], Loc["NoHitSelectedWarning"]);
+
+        private ItemsDTO<HitEntity> GetGridRows(Action<IGridColumnCollection<HitEntity>> columns,
+                QueryDictionary<StringValues> query)
+        {
+            var server = new GridServer<HitEntity>(hits, new QueryCollection(query),
+                true, "hitsGrid", columns, resultsPerPage).Sortable().Filterable().WithMultipleFilters();
+
+            // Return items to displays
+            return server.ItemsToDisplay;
+        }
     }
 }
