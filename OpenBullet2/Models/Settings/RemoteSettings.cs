000573f2ae19b22056e1551e44632038f638a28d
Ruri <ruridev@protonmail.com>
2021-01-07 17:39:36 +0100

    Added settings and UI for remotes

--- OpenBullet2/Models/Settings/RemoteSettings.cs
+++ OpenBullet2/Models/Settings/RemoteSettings.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace OpenBullet2.Models.Settings
 {
+    public class RemoteConfigsEndpoint
+    {
+        public string Url { get; set; } = "http://x.x.x.x:5000/api/shared/configs/ENDPOINT_NAME";
+        public string ApiKey { get; set; } = "MY_API_KEY";
+    }
+
     public class RemoteSettings
     {
+        public List<RemoteConfigsEndpoint> ConfigsEndpoints { get; set; } = new();
     }
 }
