81bb84ab28af795a6fa9447defd5f9f29ab5873e
Ruri <ruridev@protonmail.com>
2021-02-06 22:57:13 +0100

    Added RLHttpClient with custom request

---
+++ RuriLib.Http/Models/HttpRequest.cs
@@ -0,0 +1,154 @@
+﻿using RuriLib.Http.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RuriLib.Http.Models
+{
+    public class HttpRequest
+    {
+        public Version Version { get; set; } = new(1, 1);
+        public HttpMethod Method { get; set; } = HttpMethod.Get;
+        public Uri Uri { get; set; }
+        public Dictionary<string, string> Headers { get; set; } = new();
+        public HttpContent Content { get; set; }
+
+        public async Task<byte[]> GetBytesAsync(CookieContainer cookieContainer,
+            CancellationToken cancellationToken = default)
+        {
+            using var ms = new MemoryStream();
+            ms.Write(Encoding.ASCII.GetBytes(BuildFirstLine()));
+            ms.Write(Encoding.ASCII.GetBytes(BuildHeaders(cookieContainer)));
+
+            if (Content != null)
+            {
+                ms.Write(await Content.ReadAsByteArrayAsync(cancellationToken));
+            }
+
+            return ms.ToArray();
+        }
+
+        private static readonly string newLine = "\r\n";
+
+        /// <summary>
+        /// Safely adds a header to the dictionary.
+        /// </summary>
+        public void AddHeader(string name, string value)
+        {
+            // Make sure Host is written properly otherwise it won't get picked up below
+            if (name.Equals("Host", StringComparison.OrdinalIgnoreCase))
+            {
+                Headers["Host"] = value;
+            }
+            else
+            {
+                Headers[name] = value;
+            }
+        }
+
+        // Builds the first line, for example
+        // GET /resource HTTP/1.1
+        private string BuildFirstLine()
+            => $"{Method.Method} {Uri.PathAndQuery} HTTP/{Version}{newLine}";
+
+        // Builds the headers, for example
+        // Host: example.com
+        // Connection: Close
+        private string BuildHeaders(CookieContainer cookieContainer)
+        {
+            // NOTE: Do not use AppendLine because it appends \n instead of \r\n
+            // on Unix-like systems.
+            var sb = new StringBuilder();
+            var finalHeaders = new List<KeyValuePair<string, string>>();
+
+            // Add the Host header if not already provided
+            if (!HeaderExists("Host", out _))
+            {
+                finalHeaders.Add("Host", Uri.Host);
+            }
+
+            // Add the Connection: Close header if none is present
+            if (!HeaderExists("Connection", out _))
+            {
+                finalHeaders.Add("Connection", "Close");
+            }
+
+            // Add the non-content headers
+            foreach (var header in Headers)
+            {
+                finalHeaders.Add(header);
+            }
+
+            // Add the Cookie header if not set manually and container not null
+            if (!HeaderExists("Cookie", out _) && cookieContainer != null)
+            {
+                var cookiesCollection = cookieContainer.GetCookies(Uri);
+                if (cookiesCollection.Count > 0)
+                {
+                    var cookieBuilder = new StringBuilder();
+
+                    foreach (var cookie in cookiesCollection)
+                    {
+                        cookieBuilder
+                            .Append(cookie)
+                            .Append("; ");
+                    }
+
+                    finalHeaders.Add("Cookie", cookieBuilder);
+                }
+            }
+
+            // Add the content headers
+            if (Content != null)
+            {
+                foreach (var header in Content.Headers)
+                {
+                    // If it was already set, skip
+                    if (!HeaderExists(header.Key, out _))
+                    {
+                        finalHeaders.Add(header.Key, string.Join(' ', header.Value));
+                    }
+                }
+
+                // Add the Content-Length header if not already present
+                if (!finalHeaders.Any(h => h.Key.Equals("Content-Length", StringComparison.OrdinalIgnoreCase)))
+                {
+                    var contentLength = Content.Headers.ContentLength;
+
+                    if (contentLength.HasValue && contentLength.Value > 0)
+                    {
+                        finalHeaders.Add("Content-Length", contentLength);
+                    }
+                }
+            }
+
+            // Write all non-empty headers to the StringBuilder
+            foreach (var header in finalHeaders.Where(h => !string.IsNullOrEmpty(h.Value)))
+            {
+                sb
+                    .Append(header.Key)
+                    .Append(": ")
+                    .Append(header.Value)
+                    .Append(newLine);
+            }
+
+            // Write the final blank line after all headers
+            sb.Append(newLine);
+
+            return sb.ToString();
+        }
+
+        public bool HeaderExists(string name, out string actualName)
+        {
+            var key = Headers.Keys.FirstOrDefault(k => k.Equals(name, StringComparison.OrdinalIgnoreCase));
+            actualName = key;
+            return key != null;
+        }
+    }
+}
---
+++ RuriLib.Http/RLHttpClient.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using RuriLib.Proxies;
+using System.Text;
+using RuriLib.Proxies.Exceptions;
+using System.Collections.Generic;
+using RuriLib.Http.Models;
+
+namespace RuriLib.Http
+{
+    /// <summary>
+    /// Custom implementation of an HttpClient.
+    /// </summary>
+    public class RLHttpClient : IDisposable
+    {
+        private readonly ProxyClient proxyClient;
+
+        private Stream connectionCommonStream;
+        private NetworkStream connectionNetworkStream;
+
+        #region Properties
+        /// <summary>
+        /// The underlying proxy client.
+        /// </summary>
+        public ProxyClient ProxyClient => proxyClient;
+
+        /// <summary>
+        /// Allow automatic redirection on 3xx reply.
+        /// </summary>
+        public bool AllowAutoRedirect { get; set; } = true;
+
+        /// <summary>
+        /// The allowed SSL or TLS protocols.
+        /// </summary>
+        public SslProtocols SslProtocols { get; set; } = SslProtocols.None;
+
+        /// <summary>
+        /// If true, <see cref="AllowedCipherSuites"/> will be used instead of the default ones.
+        /// </summary>
+        public bool UseCustomCipherSuites { get; set; } = false;
+
+        /// <summary>
+        /// The cipher suites to send to the server during the TLS handshake, in order.
+        /// The default value of this property contains the cipher suites sent by Firefox as of 21 Dec 2020.
+        /// </summary>
+        public TlsCipherSuite[] AllowedCipherSuites { get; set; } = new TlsCipherSuite[]
+        {
+            TlsCipherSuite.TLS_AES_128_GCM_SHA256,
+            TlsCipherSuite.TLS_CHACHA20_POLY1305_SHA256,
+            TlsCipherSuite.TLS_AES_256_GCM_SHA384,
+            TlsCipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            TlsCipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+            TlsCipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+            TlsCipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+            TlsCipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+            TlsCipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+            TlsCipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+            TlsCipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+            TlsCipherSuite.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+            TlsCipherSuite.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+            TlsCipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256,
+            TlsCipherSuite.TLS_RSA_WITH_AES_256_GCM_SHA384,
+            TlsCipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+            TlsCipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
+            TlsCipherSuite.TLS_RSA_WITH_3DES_EDE_CBC_SHA
+        };
+
+        /// <summary>
+        /// Gets the type of decompression method used by the handler for automatic 
+        /// decompression of the HTTP content response.
+        /// </summary>
+        /// <remarks>
+        /// Support GZip and Deflate encoding automatically
+        /// </remarks>
+        public DecompressionMethods AutomaticDecompression => DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        /// <summary>
+        /// Gets or sets a value that indicates whether the handler uses the CookieContainer
+        /// property to store server cookies and uses these cookies when sending requests.
+        /// </summary>
+        public bool UseCookies { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the cookie container used to store server cookies by the handler.
+        /// </summary>
+        public CookieContainer CookieContainer { get; set; }
+
+        /// <summary>
+        /// Gets or sets delegate to verifies the remote Secure Sockets Layer (SSL) 
+        /// certificate used for authentication.
+        /// </summary>
+        public RemoteCertificateValidationCallback ServerCertificateCustomValidationCallback { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X509 certificate revocation mode.
+        /// </summary>
+        public X509RevocationMode CertRevocationMode { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RLHttpClient"/> given a <paramref name="proxyClient"/>.
+        /// </summary>
+        public RLHttpClient(ProxyClient proxyClient)
+        {
+            this.proxyClient = proxyClient ?? throw new ArgumentNullException(nameof(proxyClient));
+        }
+
+        /// <summary>
+        /// Asynchronously sends a <paramref name="request"/> and returns an <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to cancel the operation</param>
+        public async Task<HttpResponseMessage> SendAsync(HttpRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            if (UseCookies && CookieContainer == null)
+            {
+                throw new ArgumentNullException(nameof(CookieContainer));
+            }
+
+            await CreateConnection(request, cancellationToken).ConfigureAwait(false);
+            await SendDataAsync(request, cancellationToken).ConfigureAwait(false);
+            var responseMessage = await ReceiveDataAsync(request, cancellationToken).ConfigureAwait(false);
+
+            // Optionally perform auto redirection on 3xx response
+            if (((int)responseMessage.StatusCode) / 100 == 3 && AllowAutoRedirect)
+            {
+                // Compute the redirection URI
+                var redirectUri = responseMessage.Headers.Location.IsAbsoluteUri
+                    ? responseMessage.Headers.Location
+                    : new Uri(request.Uri, responseMessage.Headers.Location);
+
+                // If not 307, change the method to GET
+                if (responseMessage.StatusCode != HttpStatusCode.RedirectKeepVerb)
+                {
+                    request.Method = HttpMethod.Get;
+                    request.Content = null;
+                }
+
+                // Port over the cookies if the domains are different
+                if (request.Uri.Host != redirectUri.Host)
+                {
+                    var cookies = CookieContainer.GetCookies(request.Uri);
+                    foreach (Cookie cookie in cookies)
+                    {
+                        CookieContainer.Add(redirectUri, new Cookie(cookie.Name, cookie.Value));
+                    }
+
+                    // This is needed otherwise if the Host header was set manually
+                    // it will keep the previous one after a domain switch
+                    if (request.HeaderExists("Host", out var hostHeaderName))
+                    {
+                        request.Headers.Remove(hostHeaderName);
+                    }
+
+                    // Remove additional headers that could cause trouble
+                    request.Headers.Remove("Origin");
+                }
+
+                // Set the new URI
+                request.Uri = redirectUri;
+
+                // Perform a new request
+                return await SendAsync(request, cancellationToken).ConfigureAwait(false);
+            }
+
+            return responseMessage;
+        }
+
+        private async Task SendDataAsync(HttpRequest request, CancellationToken cancellationToken = default)
+        {
+            var buffer = await request.GetBytesAsync(CookieContainer, cancellationToken);
+            await connectionCommonStream.WriteAsync(buffer.AsMemory(0, buffer.Length), cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task<HttpResponseMessage> ReceiveDataAsync(HttpRequest request,
+            CancellationToken cancellationToken)
+        {
+            var responseBuilder = new ResponseBuilder(1024, CookieContainer, request.Uri);
+            return await responseBuilder.GetResponseAsync(null, connectionCommonStream, cancellationToken);
+        }
+
+        private async Task CreateConnection(HttpRequest request, CancellationToken cancellationToken)
+        {
+            // Get the stream from the proxies TcpClient
+            var uri = request.Uri;
+            var tcpClient = await proxyClient.ConnectAsync(uri.Host, uri.Port, null, cancellationToken);
+            connectionNetworkStream = tcpClient.GetStream();
+
+            // If https, set up a TLS stream
+            if (uri.Scheme.Equals("https", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    SslStream sslStream;
+                    sslStream = new SslStream(connectionNetworkStream, false, ServerCertificateCustomValidationCallback);
+
+                    var sslOptions = new SslClientAuthenticationOptions
+                    {
+                        TargetHost = uri.Host,
+                        EnabledSslProtocols = SslProtocols,
+                        CertificateRevocationCheckMode = CertRevocationMode
+                    };
+
+                    if (UseCustomCipherSuites)
+                        sslOptions.CipherSuitesPolicy = new CipherSuitesPolicy(AllowedCipherSuites);
+
+                    await sslStream.AuthenticateAsClientAsync(sslOptions, cancellationToken);
+                    connectionCommonStream = sslStream;
+                }
+                catch (Exception ex)
+                {
+                    if (ex is IOException || ex is AuthenticationException)
+                    {
+                        throw new ProxyException("Failed SSL connect");
+                    }
+
+                    throw;
+                }
+            }
+            else
+            {
+                connectionCommonStream = connectionNetworkStream;
+            }
+        }
+
+        public void Dispose()
+        {
+            connectionCommonStream?.Dispose();
+            connectionNetworkStream?.Dispose();
+        }
+    }
+}
