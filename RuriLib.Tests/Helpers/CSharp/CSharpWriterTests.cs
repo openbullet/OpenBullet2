862811c1a440749be275fcb372bf22eb57faa9dc
Ruri <ruridev@protonmail.com>
2021-03-13 21:33:41 +0100

    Added unit tests for SerializeInterpString

---
+++ RuriLib.Tests/Helpers/CSharp/CSharpWriterTests.cs
@@ -0,0 +1,26 @@
+﻿using RuriLib.Helpers.CSharp;
+using Xunit;
+
+namespace RuriLib.Tests.Helpers.CSharp
+{
+    public class CSharpWriterTests
+    {
+        [Fact]
+        public void SerializeInterpString_Alone_ReplaceCorrectly()
+        {
+            Assert.Equal("$\"{value}\"", CSharpWriter.SerializeInterpString("<value>"));
+        }
+
+        [Fact]
+        public void SerializeInterpString_Surrounded_ReplaceCorrectly()
+        {
+            Assert.Equal("$\"my {value} is cool\"", CSharpWriter.SerializeInterpString("my <value> is cool"));
+        }
+
+        [Fact]
+        public void SerializeInterpString_SingleCharacter_ReplaceCorrectly()
+        {
+            Assert.Equal("$\"my {a} is cool\"", CSharpWriter.SerializeInterpString("my <a> is cool"));
+        }
+    }
+}
