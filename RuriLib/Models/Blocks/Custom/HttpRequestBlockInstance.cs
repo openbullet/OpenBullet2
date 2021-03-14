openbullet/OpenBullet2

commit 66f48c14955f6f1ec3a1ee0600fd5e0f4ffa9a1c

Added urlEncodeContent setting to HttpRequest block
Closes #87

RuriLib/Models/Blocks/Custom/HttpRequestBlockInstance.cs
@@ -332,6 +332,7 @@ namespace RuriLib.Models.Blocks.Custom
             writer.Write("Method = " + GetSettingValue("method") + ", ");
             writer.Write("AutoRedirect = " + GetSettingValue("autoRedirect") + ", ");
             writer.Write("MaxNumberOfRedirects = " + GetSettingValue("maxNumberOfRedirects") + ", ");
+            writer.Write("UrlEncodeContent = " + GetSettingValue("urlEncodeContent") + ", ");
             writer.Write("AbsoluteUriInFirstLine = " + GetSettingValue("absoluteUriInFirstLine") + ", ");
             writer.Write("SecurityProtocol = " + GetSettingValue("securityProtocol") + ", ");
             writer.Write("CustomCookies = " + GetSettingValue("customCookies") + ", ");

OpenBullet2/Shared/HttpRequestBlockSettingsViewer.razor
@@ -46,6 +46,7 @@
         case StandardRequestParams x:
             <BlockInputField BlockSetting="x.Content" />
             <BlockInputField BlockSetting="x.ContentType" />
+            <BlockInputField BlockSetting="@Block.Settings["urlEncodeContent"]" />
             break;
 
         case RawRequestParams x:

RuriLib/Blocks/Requests/Http/Methods.cs
@@ -51,7 +51,20 @@ namespace RuriLib.Blocks.Requests.Http
 
             if (!string.IsNullOrEmpty(options.Content) || options.AlwaysSendContent)
             {
-                request.Content = new StringContent(options.Content.Unescape());
+                var content = options.Content;
+
+                if (options.UrlEncodeContent)
+                {
+                    // HACK: Very dirty but it works
+                    // This splits the query in chunks basing on & and =
+                    var nonce = data.Random.Next(1000000, 9999999);
+                    content = content.Replace("&", $"{nonce}&{nonce}").Replace("=", $"{nonce}={nonce}");
+                    content = string.Join("", content.SplitInChunks(2080)
+                        .Select(s => Uri.EscapeDataString(s)))
+                        .Replace($"{nonce}%26{nonce}", "&").Replace($"{nonce}%3D{nonce}", "=");
+                }
+
+                request.Content = new StringContent(content.Unescape());
                 request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(options.ContentType);
             }
 

RuriLib/Functions/Http/Options/StandardHttpRequestOptions.cs
@@ -4,5 +4,6 @@
     {
         public string Content { get; set; } = string.Empty;
         public string ContentType { get; set; } = string.Empty;
+        public bool UrlEncodeContent { get; set; } = false;
     }
 }

RuriLib/Models/Blocks/Custom/HttpRequestBlockDescriptor.cs
@@ -27,6 +27,7 @@ namespace RuriLib.Models.Blocks.Custom
                 { "method", new EnumParameter("method", typeof(HttpMethod), HttpMethod.GET.ToString()) },
                 { "autoRedirect", new BoolParameter("autoRedirect", true) },
                 { "maxNumberOfRedirects", new IntParameter("maxNumberOfRedirects", 8) },
+                { "urlEncodeContent", new BoolParameter("urlEncodeContent", false) },
                 { "absoluteUriInFirstLine", new BoolParameter("absoluteUriInFirstLine", false) },
                 { "securityProtocol", new EnumParameter("securityProtocol", typeof(SecurityProtocol), SecurityProtocol.SystemDefault.ToString()) },
                 { "useCustomCipherSuites", new BoolParameter("useCustomCipherSuites", false) },

commit 83eed47ab9d95194818a285786977bbef6fae5e6

Fixed important bug with HTTP requests due to new UrlEncode parameter
This bug was introduced in 66f48c14955f6f1ec3a1ee0600fd5e0f4ffa9a1c and it was breaking Raw, BasicAuth and Multipart requests

RuriLib/Models/Blocks/Custom/HttpRequestBlockInstance.cs
@@ -307,6 +307,7 @@ namespace RuriLib.Models.Blocks.Custom
                     writer.Write("HttpRequestStandard(data, new StandardHttpRequestOptions { ");
                     writer.Write("Content = " + CSharpWriter.FromSetting(x.Content) + ", ");
                     writer.Write("ContentType = " + CSharpWriter.FromSetting(x.ContentType) + ", ");
+                    writer.Write("UrlEncodeContent = " + GetSettingValue("urlEncodeContent") + ", ");
                     break;
 
                 case RawRequestParams x:
@@ -332,7 +333,6 @@ namespace RuriLib.Models.Blocks.Custom
             writer.Write("Method = " + GetSettingValue("method") + ", ");
             writer.Write("AutoRedirect = " + GetSettingValue("autoRedirect") + ", ");
             writer.Write("MaxNumberOfRedirects = " + GetSettingValue("maxNumberOfRedirects") + ", ");
-            writer.Write("UrlEncodeContent = " + GetSettingValue("urlEncodeContent") + ", ");
             writer.Write("AbsoluteUriInFirstLine = " + GetSettingValue("absoluteUriInFirstLine") + ", ");
             writer.Write("SecurityProtocol = " + GetSettingValue("securityProtocol") + ", ");
             writer.Write("CustomCookies = " + GetSettingValue("customCookies") + ", ");