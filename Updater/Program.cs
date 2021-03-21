2519863af0e5cbd263042ec58bfba2c6e232e069
Ruri <ruridev@protonmail.com>
2021-03-21 17:02:02 +0100

    Fixed themes being deleted on update

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -90,6 +90,18 @@ namespace Updater
             Console.Write("[5/6] Extracting the archive... ");
             try
             {
+                // Back up the themes folder
+                var themesFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/css/themes");
+                var hasThemes = Directory.Exists(themesFolder);
+                var tempFolder = Path.Combine(Path.GetTempPath(), "ob2themes");
+
+                if (hasThemes)
+                {
+                    Directory.CreateDirectory(tempFolder);
+                    DirectoryCopy(themesFolder, tempFolder, true);
+                }
+
+                // Extract the archive
                 using var archive = new ZipArchive(stream);
                 foreach (var entry in archive.Entries)
                 {
@@ -106,6 +118,14 @@ namespace Updater
 
                     }
                 }
+
+                // Restore the themes folder
+                if (hasThemes)
+                {
+                    Directory.CreateDirectory(themesFolder);
+                    DirectoryCopy(tempFolder, themesFolder, true);
+                }
+
                 Console.WriteLine("Done!");
             }
             catch (Exception ex)
@@ -137,6 +157,42 @@ namespace Updater
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
             Environment.Exit(1);
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            var dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            var dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                var tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (var subdir in dirs)
+                {
+                    var tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
         }
     }
 }
