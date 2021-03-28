e57a21b0c76f52ac309a96f2d42fb3193d9cfc99
Ruri <ruridev@protonmail.com>
2021-03-28 16:49:29 +0200

    Fixed updater failing to overwrite theme files

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -181,7 +181,7 @@ namespace Updater
             foreach (var file in files)
             {
                 var tempPath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(tempPath, false);
+                file.CopyTo(tempPath, true);
             }
 
             // If copying subdirectories, copy them and their contents to new location.
