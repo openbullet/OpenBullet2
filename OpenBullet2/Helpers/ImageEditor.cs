6fdfb1087d4891ea85359b2c2c5f09236fece838
Ruri <ruridev@protonmail.com>
2021-07-08 00:38:06 +0200

    Added support for ICO files in InputImage
    Closes #218

--- OpenBullet2/Helpers/ImageEditor.cs
+++ OpenBullet2/Helpers/ImageEditor.cs
@@ -1,16 +1,34 @@
-﻿using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Png;
+﻿using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 
 namespace OpenBullet2.Helpers
 {
     public static class ImageEditor
     {
+        public static byte[] ToCompatibleFormat(byte[] bytes)
+        {
+            // ICO magic numbers
+            if (bytes[0] == 0x00 && bytes[1] == 0x00 && bytes[2] == 0x01 && bytes[3] == 0x00)
+            {
+                using var ms = new MemoryStream(bytes);
+                var icon = new Icon(ms);
+                var bitmap = icon.ToBitmap();
+
+                using var ms2 = new MemoryStream();
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+
+            return bytes;
+        }
+
         public static string ResizeBase64(string base64, int width, int height)
         {
-            using Image image = Image.Load(Convert.FromBase64String(base64));
+            using var image = SixLabors.ImageSharp.Image.Load(Convert.FromBase64String(base64));
             
             image.Mutate(x => x
                 .Resize(width, height));
