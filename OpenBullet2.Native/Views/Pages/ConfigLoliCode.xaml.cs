2c4b97a437f181b59361bf9c883ce60f56d15812
Ruri <ruridev@protonmail.com>
2021-08-15 15:05:34 +0200

    Save on CTRL+S

--- OpenBullet2.Native/Views/Pages/ConfigLoliCode.xaml.cs
+++ OpenBullet2.Native/Views/Pages/ConfigLoliCode.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using OpenBullet2.Core.Repositories;
 using OpenBullet2.Core.Services;
+using OpenBullet2.Native.Helpers;
 using OpenBullet2.Native.Views.Pages.Shared;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Xml;
 
@@ -15,21 +18,20 @@ namespace OpenBullet2.Native.Views.Pages
     public partial class ConfigLoliCode : Page
     {
         private readonly ConfigService configService;
+        private readonly IConfigRepository configRepo; // TODO: This should not be here
 
         public ConfigLoliCode()
         {
             InitializeComponent();
             configService = SP.GetService<ConfigService>();
+            configRepo = SP.GetService<IConfigRepository>();
             debuggerFrame.Content = SP.GetService<Debugger>();
 
             HighlightSyntax();
             UpdateViewModel();
         }
 
-        public void UpdateViewModel()
-        {
-            editor.Text = configService.SelectedConfig.LoliCodeScript;
-        }
+        public void UpdateViewModel() => editor.Text = configService.SelectedConfig.LoliCodeScript;
 
         private void EditorLostFocus(object sender, RoutedEventArgs e) 
             => configService.SelectedConfig.LoliCodeScript = editor.Text;
@@ -40,6 +42,17 @@ namespace OpenBullet2.Native.Views.Pages
             editor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
             editor.TextArea.TextView.LinkTextForegroundBrush = new SolidColorBrush(Colors.DodgerBlue);
             editor.TextArea.TextView.LinkTextUnderline = false;
+        }
+
+        private async void PageKeyDown(object sender, KeyEventArgs e)
+        {
+            // Save on CTRL+S
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.S)
+            {
+                configService.SelectedConfig.LoliCodeScript = editor.Text;
+                await configRepo.Save(configService.SelectedConfig);
+                Alert.Info("Saved", $"{configService.SelectedConfig.Metadata.Name} was saved successfully!");
+            }
         }
     }
 }
