decc0315047caf91db5aca98d621e3eda31bf68c
Ruri <ruridev@protonmail.com>
2021-08-15 20:54:40 +0200

    Toggle usings

--- OpenBullet2.Native/Views/Pages/ConfigLoliCode.xaml.cs
+++ OpenBullet2.Native/Views/Pages/ConfigLoliCode.xaml.cs
@@ -2,10 +2,12 @@
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 using OpenBullet2.Core.Repositories;
 using OpenBullet2.Core.Services;
+using OpenBullet2.Native.Extensions;
 using OpenBullet2.Native.Helpers;
 using OpenBullet2.Native.Views.Pages.Shared;
 using RuriLib.Models.Configs;
 using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -21,6 +23,7 @@ namespace OpenBullet2.Native.Views.Pages
     {
         private readonly ConfigService configService;
         private readonly IConfigRepository configRepo; // TODO: This should not be here
+        private Config Config => configService.SelectedConfig;
 
         public ConfigLoliCode()
         {
@@ -40,6 +43,8 @@ namespace OpenBullet2.Native.Views.Pages
                 // Try to change the mode to LoliCode and set the editor's text
                 configService.SelectedConfig.ChangeMode(ConfigMode.LoliCode);
                 editor.Text = configService.SelectedConfig.LoliCodeScript;
+                usingsRTB.Document.Blocks.Clear();
+                usingsRTB.AppendText(string.Join(Environment.NewLine, Config.Settings.ScriptSettings.CustomUsings), Colors.White);
             }
             catch (Exception ex)
             {
@@ -70,5 +75,11 @@ namespace OpenBullet2.Native.Views.Pages
                 Alert.Info("Saved", $"{configService.SelectedConfig.Metadata.Name} was saved successfully!");
             }
         }
+
+        private void ToggleUsings(object sender, RoutedEventArgs e) => usingsRTB.Visibility = 
+            usingsRTB.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+
+        private void UsingsChanged(object sender, TextChangedEventArgs e)
+            => Config.Settings.ScriptSettings.CustomUsings = usingsRTB.Lines().Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
     }
 }
