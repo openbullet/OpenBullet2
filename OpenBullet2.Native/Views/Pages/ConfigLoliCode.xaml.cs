d8a58305a9be33cdf5d339f49a58777843b329db
Ruri <ruridev@protonmail.com>
2021-08-25 19:03:40 +0200

    Fixed usings

--- OpenBullet2.Native/Views/Pages/ConfigLoliCode.xaml.cs
+++ OpenBullet2.Native/Views/Pages/ConfigLoliCode.xaml.cs
@@ -2,9 +2,8 @@
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 using OpenBullet2.Core.Repositories;
 using OpenBullet2.Core.Services;
-using OpenBullet2.Native.Extensions;
 using OpenBullet2.Native.Helpers;
-using OpenBullet2.Native.Views.Pages.Shared;
+using OpenBullet2.Native.ViewModels;
 using RuriLib.Models.Configs;
 using System;
 using System.Linq;
@@ -21,12 +20,16 @@ namespace OpenBullet2.Native.Views.Pages
     /// </summary>
     public partial class ConfigLoliCode : Page
     {
+        private readonly ConfigLoliCodeViewModel vm;
         private readonly ConfigService configService;
         private readonly IConfigRepository configRepo; // TODO: This should not be here
         private Config Config => configService.SelectedConfig;
 
         public ConfigLoliCode()
         {
+            vm = new ConfigLoliCodeViewModel();
+            DataContext = vm;
+
             InitializeComponent();
             configService = SP.GetService<ConfigService>();
             configRepo = SP.GetService<IConfigRepository>();
@@ -41,8 +44,7 @@ namespace OpenBullet2.Native.Views.Pages
                 // Try to change the mode to LoliCode and set the editor's text
                 configService.SelectedConfig.ChangeMode(ConfigMode.LoliCode);
                 editor.Text = configService.SelectedConfig.LoliCodeScript;
-                usingsRTB.Document.Blocks.Clear();
-                usingsRTB.AppendText(string.Join(Environment.NewLine, Config.Settings.ScriptSettings.CustomUsings), Colors.White);
+                vm.UpdateViewModel();
             }
             catch (Exception ex)
             {
@@ -74,10 +76,28 @@ namespace OpenBullet2.Native.Views.Pages
             }
         }
 
-        private void ToggleUsings(object sender, RoutedEventArgs e) => usingsRTB.Visibility = 
-            usingsRTB.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        private void ToggleUsings(object sender, RoutedEventArgs e) => usingsTextBox.Visibility =
+            usingsTextBox.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+    }
 
-        private void UsingsChanged(object sender, TextChangedEventArgs e)
-            => Config.Settings.ScriptSettings.CustomUsings = usingsRTB.Lines().Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+    public class ConfigLoliCodeViewModel : ViewModelBase
+    {
+        private readonly ConfigService configService;
+        private Config Config => configService.SelectedConfig;
+
+        public ConfigLoliCodeViewModel()
+        {
+            configService = SP.GetService<ConfigService>();
+        }
+
+        public string UsingsString
+        {
+            get => string.Join(Environment.NewLine, Config.Settings.ScriptSettings.CustomUsings);
+            set
+            {
+                Config.Settings.ScriptSettings.CustomUsings = value.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+                OnPropertyChanged();
+            }
+        }
     }
 }
