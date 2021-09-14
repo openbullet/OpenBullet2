540100c78abac41c7bbc548afd1d8e8955cda9bc
Ruri <ruridev@protonmail.com>
2021-09-14 20:54:36 +0200

    Fixed an issue with copying to clipboard
    Closes #469

--- OpenBullet2.Native/Extensions/IEnumerableExtensions.cs
+++ OpenBullet2.Native/Extensions/IEnumerableExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace OpenBullet2.Native.Extensions
@@ -18,7 +19,29 @@ namespace OpenBullet2.Native.Extensions
             File.WriteAllLines(fileName, items.Select(i => mapping(i)));
         }
 
-        public static void CopyToClipboard<T>(this IEnumerable<T> items, Func<T, string> mapping) 
-            => Clipboard.SetText(string.Join(Environment.NewLine, items.Select(i => mapping(i))));
+        public static void CopyToClipboard<T>(this IEnumerable<T> items, Func<T, string> mapping)
+        {
+            // Need to run the loop a few times otherwise sometimes it throws and says CLIPBRD_E_CANT_OPEN
+            // https://stackoverflow.com/questions/68666/clipbrd-e-cant-open-error-when-setting-the-clipboard-from-net
+            for (var i = 0; i < 10; i++)
+            {
+                try
+                {
+                    Clipboard.SetText(string.Join(Environment.NewLine, items.Select(i => mapping(i))));
+                    return;
+                }
+                catch (COMException ex)
+                {
+                    const uint CLIPBRD_E_CANT_OPEN = 0x800401D0;
+
+                    if ((uint)ex.ErrorCode != CLIPBRD_E_CANT_OPEN)
+                    {
+                        throw;
+                    }
+                }
+
+                System.Threading.Thread.Sleep(10);
+            }
+        }
     }
 }
