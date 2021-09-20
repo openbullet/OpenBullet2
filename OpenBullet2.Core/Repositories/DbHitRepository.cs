5ef52da93245a4e3f28206b61ef659276102d7df
Ruri <ruridev@protonmail.com>
2021-09-20 12:17:32 +0200

    Fixed a bug when obtaining hits
    Closes #478

--- OpenBullet2.Core/Repositories/DbHitRepository.cs
+++ OpenBullet2.Core/Repositories/DbHitRepository.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OpenBullet2.Core.Entities;
+using OpenBullet2.Core.Extensions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace OpenBullet2.Core.Repositories
 {
@@ -8,7 +11,7 @@ namespace OpenBullet2.Core.Repositories
     /// </summary>
     public class DbHitRepository : DbRepository<HitEntity>, IHitRepository
     {
-        public DbHitRepository(ApplicationDbContext context) 
+        public DbHitRepository(ApplicationDbContext context)
             : base(context)
         {
 
@@ -16,5 +19,23 @@ namespace OpenBullet2.Core.Repositories
 
         /// <inheritdoc/>
         public void Purge() => _ = context.Database.ExecuteSqlRaw($"DELETE FROM {nameof(ApplicationDbContext.Hits)}");
+
+        public async override Task Update(HitEntity entity)
+        {
+            context.DetachLocal<HitEntity>(entity.Id);
+            context.Entry(entity).State = EntityState.Modified;
+            await base.Update(entity);
+        }
+
+        public async override Task Update(IEnumerable<HitEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                context.DetachLocal<HitEntity>(entity.Id);
+                context.Entry(entity).State = EntityState.Modified;
+            }
+
+            await base.Update(entities);
+        }
     }
 }
