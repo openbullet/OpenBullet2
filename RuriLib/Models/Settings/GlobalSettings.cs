5732a8548c3267458b4aa2b7faf3fbcabb565848
Ruri <ruridev@protonmail.com>
2021-10-25 01:33:28 +0200

    Selenium settings in RLsettings

--- RuriLib/Models/Settings/GlobalSettings.cs
+++ RuriLib/Models/Settings/GlobalSettings.cs
@@ -6,5 +6,6 @@
         public CaptchaSettings CaptchaSettings { get; set; } = new CaptchaSettings();
         public ProxySettings ProxySettings { get; set; } = new ProxySettings();
         public PuppeteerSettings PuppeteerSettings { get; set; } = new PuppeteerSettings();
+        public SeleniumSettings SeleniumSettings { get; set; } = new SeleniumSettings();
     }
 }
