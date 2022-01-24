be9bbca5602dd5f1c15ae0b62136069b0eb6593d
Ruri <ruridev@protonmail.com>
2022-01-24 18:22:02 +0100

    Added new attributes

---
+++ RuriLib/Attributes/AsImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RuriLib.Attributes
+{
+    /// <summary>
+    /// Attribute used to decorate a parameter of type <see cref="byte[]"/> that
+    /// is supposed to be an image.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class AsImage : Attribute
+    {
+
+    }
+}
---
+++ RuriLib/Attributes/BlockAction.cs
@@ -0,0 +1,33 @@
+﻿using RuriLib.Models.Blocks;
+using System;
+
+namespace RuriLib.Attributes
+{
+    /// <summary>
+    /// Attribute used to decorate a block that supports an action.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class BlockAction : Attribute
+    {
+        /// <summary>
+        /// The name of this action.
+        /// </summary>
+        public string name = null;
+
+        /// <summary>
+        /// The action to call. The instanced block will be provided as the
+        /// only argument.
+        /// </summary>
+        public Action<BlockInstance> action = null;
+
+        /// <summary>
+        /// Creates a <see cref="BlockAction"/> given the <paramref name="name"/>
+        /// and the <paramref name="action"/>.
+        /// </summary>
+        public BlockAction(string name, Action<BlockInstance> action)
+        {
+            this.name = name;
+            this.action = action;
+        }
+    }
+}
