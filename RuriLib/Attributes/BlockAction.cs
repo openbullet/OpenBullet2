fb725f2c624b8a4dded4a4649f37d65e9a32ae6f
Ruri <ruridev@protonmail.com>
2022-01-29 00:20:34 +0100

    Added BlockAction and BlockImage attributes and logic for web client

--- RuriLib/Attributes/BlockAction.cs
+++ RuriLib/Attributes/BlockAction.cs
@@ -1,33 +1,44 @@
-﻿using RuriLib.Models.Blocks;
-using System;
+﻿using System;
 
 namespace RuriLib.Attributes
 {
     /// <summary>
-    /// Attribute used to decorate a block that supports an action.
+    /// Attribute used to decorate a method that is a block action. The method should take only one
+    /// parameter of type <see cref="Models.Blocks.BlockInstance"/>.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class BlockAction : Attribute
     {
         /// <summary>
-        /// The name of this action.
+        /// The name of the action. If not specified, a name will automatically be 
+        /// generated from the name of the method.
         /// </summary>
         public string name = null;
 
         /// <summary>
-        /// The action to call. The instanced block will be provided as the
-        /// only argument.
+        /// The description of what the action does.
         /// </summary>
-        public Action<BlockInstance> action = null;
+        public string description = null;
 
         /// <summary>
-        /// Creates a <see cref="BlockAction"/> given the <paramref name="name"/>
-        /// and the <paramref name="action"/>.
+        /// The id of the block to which this action belongs to. Normally, the
+        /// id of a block is the name of the method.
         /// </summary>
-        public BlockAction(string name, Action<BlockInstance> action)
+        public string parentBlockId = null;
+
+        /// <summary>
+        /// Defines a block action.
+        /// </summary>
+        /// <param name="parentBlockId">The id of the block to which this action belongs to. Normally, the
+        /// id of a block is the name of the method.</param>
+        /// <param name="name">The name of the action. If not specified, a name will automatically be 
+        /// generated from the name of the method.</param>
+        /// <param name="description">The description of what the action does.</param>
+        public BlockAction(string parentBlockId, string name = null, string description = null)
         {
+            this.parentBlockId = parentBlockId;
             this.name = name;
-            this.action = action;
+            this.description = description;
         }
     }
 }
---
+++ RuriLib/Models/Blocks/BlockActionInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace RuriLib.Models.Blocks
+{
+    public delegate void BlockActionDelegate(BlockInstance block);
+
+    public class BlockActionInfo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public BlockActionDelegate Delegate { get; set; }
+    }
+}
