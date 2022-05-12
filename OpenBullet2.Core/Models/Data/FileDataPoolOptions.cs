0a9bc63f7b40e7e1d7545e2f1805b0923988d232
Ruri <ruridev@protonmail.com>
2022-05-12 08:17:50 +0200

    Fixed FileDataPool not accepting double quoted paths on Windows
    Closes #585

--- OpenBullet2.Core/Models/Data/FileDataPoolOptions.cs
+++ OpenBullet2.Core/Models/Data/FileDataPoolOptions.cs
@@ -1,4 +1,5 @@
 ﻿using RuriLib.Models.Data.DataPools;
+using System.Runtime.InteropServices;
 
 namespace OpenBullet2.Core.Models.Data
 {
@@ -7,10 +8,30 @@ namespace OpenBullet2.Core.Models.Data
     /// </summary>
     public class FileDataPoolOptions : DataPoolOptions
     {
+        private string fileName = null;
+
         /// <summary>
         /// The path to the file on disk.
         /// </summary>
-        public string FileName { get; set; }
+        public string FileName
+        { 
+            get => fileName;
+            set
+            {
+                // Double quotes in file names are not allowed in Windows, but they are included
+                // at the start and end of the file path if you copy/paste it from some programs,
+                // so we need to remove them, otherwise it will not find the file.
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    // Remove the double quotes from the file
+                    fileName = value.Replace("\"", "");
+                }
+                else
+                {
+                    fileName = value;
+                }
+            }
+        }
 
         /// <summary>
         /// The Wordlist Type.
