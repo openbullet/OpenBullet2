0a1433d7fe486d1749b1796afefe7bfc8d6f457a
Ruri <ruridev@protonmail.com>
2023-01-31 23:45:54 +0100

    Allowed anon requests if require admin login is disabled

--- OpenBullet2.Web/Exceptions/AuthTokenVerificationMiddleware.cs
+++ OpenBullet2.Web/Exceptions/AuthTokenVerificationMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using OpenBullet2.Web.Extensions;
+﻿using OpenBullet2.Core.Services;
+using OpenBullet2.Web.Extensions;
 using OpenBullet2.Web.Interfaces;
 using OpenBullet2.Web.Models.Identity;
 using System.Security.Claims;
@@ -9,19 +10,39 @@ internal class AuthTokenVerificationMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger<AuthTokenVerificationMiddleware> _logger;
+    private readonly OpenBulletSettingsService _obSettingsService;
     private readonly IAuthTokenService _authTokenService;
 
     public AuthTokenVerificationMiddleware(RequestDelegate next,
         ILogger<AuthTokenVerificationMiddleware> logger,
+        OpenBulletSettingsService obSettingsService,
         IAuthTokenService authTokenService)
     {
         _next = next;
         _logger = logger;
+        _obSettingsService = obSettingsService;
         _authTokenService = authTokenService;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
+        // If the admin user does not need any login, allow anonymous requests
+        if (!_obSettingsService.Settings.SecuritySettings.RequireAdminLogin)
+        {
+            var apiUser = new ApiUser
+            {
+                Id = -1,
+                Role = UserRole.Admin,
+                Username = _obSettingsService.Settings.SecuritySettings.AdminUsername
+            };
+
+            context.SetApiUser(apiUser);
+
+            await _next(context);
+
+            return;
+        }
+
         // If there is a token, validate it
         var token = context.Request.Headers["Authorization"]
             .FirstOrDefault()?.Split(' ').Last();
