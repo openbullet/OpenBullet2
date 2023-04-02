babed11afae6d9be4d380de3636e6c20e6cf8fd8
Ruri <ruridev@protonmail.com>
2023-04-02 02:17:29 +0200

    Added primeng, styles and animations

--- openbullet2-web-client/src/app/app.module.ts
+++ openbullet2-web-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
