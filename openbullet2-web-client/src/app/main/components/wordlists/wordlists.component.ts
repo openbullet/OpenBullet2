59ddf371a31d2775caaf2c931c6df5a5c6f0be4d
Ruri <ruridev@protonmail.com>
2023-06-03 13:38:25 +0200

    Worked on wordlists component

--- openbullet2-web-client/src/app/main/components/wordlists/wordlists.component.ts
+++ openbullet2-web-client/src/app/main/components/wordlists/wordlists.component.ts
@@ -1,10 +1,159 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WordlistDto } from '../../dtos/wordlist/wordlist.dto';
+import { faFilterCircleXmark, faPen, faX } from '@fortawesome/free-solid-svg-icons';
+import { ConfirmationService, MenuItem, MessageService } from 'primeng/api';
+import { WordlistService } from '../../services/wordlist.service';
+import { SettingsService } from '../../services/settings.service';
+import { EnvironmentSettingsDto } from '../../dtos/settings/environment-settings.dto';
+import { UpdateWordlistInfoDto } from '../../dtos/wordlist/update-wordlist-info.dto';
 
 @Component({
   selector: 'app-wordlists',
   templateUrl: './wordlists.component.html',
   styleUrls: ['./wordlists.component.scss']
 })
-export class WordlistsComponent {
+export class WordlistsComponent implements OnInit {
+  envSettings: EnvironmentSettingsDto | null = null;
+  wordlists: WordlistDto[] | null = null;
 
+  faPen = faPen;
+  faX = faX;
+  faFilterCircleXmark = faFilterCircleXmark;
+
+  selectedWordlist: WordlistDto | null = null;
+  addWordlistModalVisible = false;
+  uploadWordlistModalVisible = false;
+  updateWordlistInfoModalVisible = false;
+
+  wordlistMenuItems: MenuItem[] = [
+    {
+      id: 'add',
+      label: 'Add',
+      icon: 'pi pi-fw pi-plus',
+      items: [
+        {
+          id: 'add-from-local-file',
+          label: 'From local file',
+          icon: 'pi pi-fw pi-file color-good',
+          command: e => this.openAddWordlistModal()
+        },
+        {
+          id: 'upload-file',
+          label: 'Upload (remote)',
+          icon: 'pi pi-fw pi-upload color-good',
+          command: e => this.openUploadWordlistModal()
+        }
+      ]
+    },
+    {
+      id: 'delete',
+      label: 'Delete',
+      icon: 'pi pi-fw pi-trash',
+      items: [
+        {
+          id: 'delete-not-found',
+          label: 'With missing files',
+          icon: 'pi pi-fw pi-trash color-bad',
+          command: e => this.confirmDeleteNotFound()
+        }
+      ]
+    }
+  ];
+
+  constructor(private wordlistService: WordlistService,
+    private settingsService: SettingsService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService) {
+
+  }
+
+  ngOnInit(): void {
+    this.settingsService.getEnvironmentSettings()
+      .subscribe(envSettings => {
+        this.envSettings = envSettings;
+        this.refreshWordlists();
+      });
+  }
+
+  refreshWordlists() {
+    if (this.envSettings === null) return;
+
+    this.wordlistService.getAllWordlists()
+      .subscribe(wordlists => this.wordlists = wordlists);
+  }
+
+  openAddWordlistModal() {
+    this.addWordlistModalVisible = true;
+  }
+
+  openUploadWordlistModal() {
+    this.uploadWordlistModalVisible = true;
+  }
+
+  openUpdateWordlistInfoModal(wordlist: WordlistDto) {
+    this.selectedWordlist = wordlist;
+    this.updateWordlistInfoModalVisible = true;
+  }
+
+  updateWordlistInfo(updated: UpdateWordlistInfoDto){
+    this.wordlistService.updateWordlistInfo(updated)
+      .subscribe(resp => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Updated',
+          detail: `Wordlist ${resp.name} was updated`
+        });
+        this.updateWordlistInfoModalVisible = false;
+        this.refreshWordlists();
+      });
+  }
+
+  confirmDeleteWordlist(wordlist: WordlistDto) {
+    this.confirmationService.confirm({
+      message: `In addition to deleting the wordlist, do you also
+      want to delete the file that it references from disk? The original
+      file will be permanently deleted from disk.`,
+      header: 'Also delete the file?',
+      acceptLabel: 'Yes, delete the file',
+      rejectLabel: 'No, just the wordlist',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => this.deleteWordlist(wordlist, true),
+      reject: () => this.deleteWordlist(wordlist, false)
+    });
+  }
+
+  deleteWordlist(wordlist: WordlistDto, alsoDeleteFile: boolean) {
+    this.wordlistService.deleteWordlist(wordlist.id, alsoDeleteFile)
+    .subscribe(resp => {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Delete',
+        detail: `Wordlist ${wordlist.name} was deleted` +
+          alsoDeleteFile ? ', along with its file' : ''
+      });
+      this.refreshWordlists();
+    });
+  }
+
+  confirmDeleteNotFound() {
+    this.confirmationService.confirm({
+      message: `You are about to delete all wordlists that reference 
+      a file that can no longer be found (e.g. it was deleted).
+      Are you sure that you want to proceed?`,
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => this.deleteNotFound()
+    });
+  }
+
+  deleteNotFound() {
+    this.wordlistService.deleteNotFoundWordlists()
+      .subscribe(resp => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Deleted',
+          detail: `${resp.count} wordlists were deleted from the database`
+        });
+      });
+  }
 }
