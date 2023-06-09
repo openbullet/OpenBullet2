6224d061fecfbdb93e3e85423a864a924db79564
Ruri <ruridev@protonmail.com>
2023-06-10 00:26:53 +0200

    Changed base host to https localhost 5001

--- openbullet2-web-client/src/app/shared/utils/host.ts
+++ openbullet2-web-client/src/app/shared/utils/host.ts
@@ -1,9 +1,9 @@
 export function getBaseUrl() {
     // Gets the base URL for API calls
     // return window.location.origin + '/api/v1';
-    return 'http://localhost:5133/api/v1';
+    return 'https://localhost:5001/api/v1';
 }
 
 export function getBaseHubUrl() {
-    return 'http://localhost:5133/hubs';
+    return 'https://localhost:5001/hubs';
 }
