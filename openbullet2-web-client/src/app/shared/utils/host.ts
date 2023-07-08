158090095102e9cfd59de6a0fcf0c2eb88c3e2dc
Ruri <ruridev@protonmail.com>
2023-07-08 15:19:07 +0200

    Changed api endpoint to http or firefox does not work

--- openbullet2-web-client/src/app/shared/utils/host.ts
+++ openbullet2-web-client/src/app/shared/utils/host.ts
@@ -1,9 +1,9 @@
 export function getBaseUrl() {
     // Gets the base URL for API calls
     // return window.location.origin + '/api/v1';
-    return 'https://localhost:5001/api/v1';
+    return 'http://localhost:5000/api/v1';
 }
 
 export function getBaseHubUrl() {
-    return 'https://localhost:5001/hubs';
+    return 'http://localhost:5000/hubs';
 }
