9fb7ad01fee234901e102105f21b91c96a7c7b19
Ruri <ruridev@protonmail.com>
2023-08-27 01:52:55 +0200

    First sketch of job manager

--- openbullet2-web-client/src/app/main/components/jobs/jobs.component.ts
+++ openbullet2-web-client/src/app/main/components/jobs/jobs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { faBolt, faPlus, faX } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { faBolt, faClone, faPen, faPlay, faPlus, faStop, faX } from '@fortawesome/free-solid-svg-icons';
 import { JobService } from '../../services/job.service';
 import { MultiRunJobOverviewDto } from '../../dtos/job/multi-run-job-overview.dto';
 import { ProxyCheckJobOverviewDto } from '../../dtos/job/proxy-check-job-overview.dto';
@@ -9,13 +9,19 @@ import { ProxyCheckJobOverviewDto } from '../../dtos/job/proxy-check-job-overvie
   templateUrl: './jobs.component.html',
   styleUrls: ['./jobs.component.scss']
 })
-export class JobsComponent {
+export class JobsComponent implements OnInit, OnDestroy {
   multiRunJobs: MultiRunJobOverviewDto[] | null = null;
   proxyCheckJobs: ProxyCheckJobOverviewDto[] | null = null;
 
   faBolt = faBolt;
   faPlus = faPlus;
   faX = faX;
+  faStop = faStop;
+  faPlay = faPlay;
+  faPen = faPen;
+  faClone = faClone;
+
+  intervalId: any;
 
   constructor(private jobService: JobService) {
 
@@ -23,6 +29,14 @@ export class JobsComponent {
 
   ngOnInit(): void {
     this.refreshJobs();
+
+    this.intervalId = setInterval(() => {
+      this.refreshJobs();
+    }, 1000); // TODO: This number should be read from settings
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
   }
 
   refreshJobs() {
@@ -38,6 +52,34 @@ export class JobsComponent {
   }
 
   confirmRemoveAllJobs() {
+
+  }
+
+  abortJob(job: MultiRunJobOverviewDto | ProxyCheckJobOverviewDto) {
+
+  }
+
+  startJob(job: MultiRunJobOverviewDto | ProxyCheckJobOverviewDto) {
+
+  }
+
+  editMultiRunJob(job: MultiRunJobOverviewDto) {
+
+  }
+
+  editProxyCheckJob(job: ProxyCheckJobOverviewDto) {
+
+  }
+
+  cloneMultiRunJob(job: MultiRunJobOverviewDto) {
+
+  }
+
+  cloneProxyCheckJob(job: ProxyCheckJobOverviewDto) {
+
+  }
+
+  removeJob(job: MultiRunJobOverviewDto | ProxyCheckJobOverviewDto) {
 
   }
 }
