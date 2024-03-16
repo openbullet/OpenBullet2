e75321211e3d6d2bc03b6c5c6affd48d9808f3e0
Ruri <ruridev@protonmail.com>
2024-03-16 21:38:15 +0100

    Allowed CTRL+LMB on menu items

--- openbullet2-web-client/src/app/main/components/menu/menu-item/menu-item.component.ts
+++ openbullet2-web-client/src/app/main/components/menu/menu-item/menu-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -12,7 +13,18 @@ export class MenuItemComponent {
   @Input() icon!: IconDefinition;
   @Input() active!: boolean;
 
+  constructor(private router: Router) { }
+
   getItemClass(): string {
     return this.active ? 'menu-item selected' : 'menu-item';
+  }
+
+  navigate(e: MouseEvent): void {
+    // If ctrl + left click, open in new tab
+    if (e.ctrlKey) {
+      window.open(this.link, '_blank');
+    } else {
+      this.router.navigate([this.link]);
+    }
   }
 }
