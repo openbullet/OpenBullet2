ec56859b3802f5950dcf7b1aca3306960e8c8cdb
Ruri <ruridev@protonmail.com>
2024-04-21 12:08:06 +0200

    [build] Fixed editor not updating when switching between LoliCode blocks

--- openbullet2-web-client/src/app/main/components/config/config-stacker/lolicode-block/lolicode-block.component.ts
+++ openbullet2-web-client/src/app/main/components/config/config-stacker/lolicode-block/lolicode-block.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { BlockDescriptorDto } from 'src/app/main/dtos/config/block-descriptor.dto';
 import { LoliCodeBlockInstanceDto } from 'src/app/main/dtos/config/block-instance.dto';
 import { CodeEditorComponent } from 'src/app/shared/components/code-editor/code-editor.component';
@@ -8,7 +8,7 @@ import { CodeEditorComponent } from 'src/app/shared/components/code-editor/code-
   templateUrl: './lolicode-block.component.html',
   styleUrls: ['./lolicode-block.component.scss'],
 })
-export class LolicodeBlockComponent {
+export class LolicodeBlockComponent implements OnChanges {
   @Input() block!: LoliCodeBlockInstanceDto;
   @Input() descriptor!: BlockDescriptorDto;
 
@@ -16,6 +16,14 @@ export class LolicodeBlockComponent {
 
   @ViewChild('editor')
   editor: CodeEditorComponent | undefined = undefined;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // We need this when switching between two Lolicode blocks,
+    // otherwise the editor will not update the code.
+    if (this.editor !== undefined) {
+      this.editor.code = this.block.script;
+    }
+  }
 
   valueChanged() {
     this.onChange.emit();
