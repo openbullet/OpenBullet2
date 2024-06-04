95280fb501f66cde600a1ade4aaef3ec4282431a
Ruri <ruridev@protonmail.com>
2024-06-04 15:26:33 +0200

    Refactored updater code and added check for asp.net core runtime 8.0+

---
+++ Updater/Helpers/RequirementsChecker.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CliWrap;
+using CliWrap.Buffered;
+
+namespace Updater.Helpers;
+
+public static class RequirementsChecker
+{
+    public static void EnsureOb2WebNotRunning()
+    {
+        if (Process.GetProcessesByName("OpenBullet2.Web").Length > 0)
+        {
+            Utils.ExitWithError("OpenBullet 2 is currently running, please close it before updating!");
+        }
+    }
+
+    /// <summary>
+    /// Checks if the ASP.NET Core Runtime 8.0+ is installed. If the user installed the SDK,
+    /// it will still work because the runtime is included in the SDK.
+    /// </summary>
+    public static async Task EnsureDotNetInstalledAsync()
+    {
+        try
+        {
+            // If dotnet is not a valid command, this will throw an exception
+            var result = await Cli.Wrap("dotnet")
+                .WithArguments("--list-runtimes")
+                .WithValidation(CommandResultValidation.None)
+                .ExecuteBufferedAsync();
+            
+            // The output of the command is something like:
+            // Microsoft.AspNetCore.App 8.0.0 [/usr/local/share/dotnet/shared/Microsoft.AspNetCore.App]
+            // Microsoft.NETCore.App 8.0.0 [/usr/local/share/dotnet/shared/Microsoft.NETCore.App]
+            
+            if (result.ExitCode == 0)
+            {
+                if (!result.StandardOutput.Split('\n').Any(line => line.StartsWith("Microsoft.AspNetCore.App 8.")))
+                {
+                    Utils.ExitWithError("The ASP.NET Core Runtime version 8.0 or higher is required to run OpenBullet 2. " +
+                                        "Please install it and relaunch the Updater");
+                }
+            }
+            else
+            {
+                Utils.ExitWithError("The ASP.NET Core Runtime version 8.0 or higher was not found on your system. " +
+                                    "Please install it and relaunch the Updater");
+            }
+        }
+        catch (Exception)
+        {
+            Utils.ExitWithError("The ASP.NET Core Runtime version 8.0 or higher was not found on your system. " +
+                                "Please install it and relaunch the Updater");
+        }
+    }
+}
--- Updater/Program.cs
+++ Updater/Program.cs
@@ -1,125 +1,48 @@
 ﻿using CommandLine;
-using Newtonsoft.Json.Linq;
 using Spectre.Console;
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.IO.Compression;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Updater.Helpers;
 
 namespace Updater
 {
-    public class Program
+    public static class Program
     {
-        private static async Task Main(string[] args) =>
-            // Parse the Options
-            await new Parser(with =>
-                {
-                    with.CaseInsensitiveEnumValues = true;
-                }).ParseArguments<CliOptions>(args)
-                .WithParsedAsync(async opts => await UpdateAsync(opts));
+        private static async Task Main(string[] args)
+        {
+            try
+            {
+                await new Parser(with => { with.CaseInsensitiveEnumValues = true; }).ParseArguments<CliOptions>(args)
+                    .WithParsedAsync(async opts => await UpdateAsync(opts));
+            }
+            catch (Exception ex)
+            {
+                Utils.ExitWithError(ex);
+            }
+        }
 
         private static async Task UpdateAsync(CliOptions options)
         {
-            // Make sure the repository is in the right format
-            if (!Regex.IsMatch(options.Repository, @"^[\w-]+/[\w-]+$"))
-            {
-                ExitWithError("The repository must be in the format owner/repo (e.g. openbullet/OpenBullet2)");
-                return;
-            }
+            // Validate the repository
+            InputValidation.ValidateRepository(options.Repository);
             
             // If the channel was not specified, ask the user
-            if (options.Channel is null)
-            {
-                var response = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                        .Title("Please select the channel")
-                        .PageSize(3)
-                        .AddChoices(["Staging (early builds)", "Release (stable builds)"]));
-                
-                options.Channel = response switch
-                {
-                    "Staging (early builds)" => BuildChannel.Staging,
-                    "Release (stable builds)" => BuildChannel.Release,
-                    _ => BuildChannel.Release
-                };
-            }
+            options.Channel ??= Channel.AskForChannel();
             
-            using HttpClient client = new();
-            client.BaseAddress = new Uri($"https://api.github.com/repos/{options.Repository}/");
-            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+            // Check if OpenBullet2 is running
+            RequirementsChecker.EnsureOb2WebNotRunning();
             
-            AnsiConsole.MarkupLine($"[yellow]Checking for updates for {options.Repository} on the {options.Channel} channel...[/]");
-            
-            if (!string.IsNullOrWhiteSpace(options.Username) && !string.IsNullOrWhiteSpace(options.Token))
-            {
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                    "Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{options.Username}:{options.Token}")));
-                AnsiConsole.MarkupLine("[yellow]Using authentication...[/]");
-            }
+            // Make sure the user has Microsoft.AspNetCore.App 8.0 or higher installed
+            await RequirementsChecker.EnsureDotNetInstalledAsync();
 
-            var release = JToken.Parse("{}");
-            var remoteVersion = new Version();
-            
             // Fetch info from remote
-            await AnsiConsole.Status()
-                .StartAsync("[yellow]Fetching version info from remote...[/]", async ctx => 
-                {
-                    try
-                    {
-                        // Query the github api to get a list of the latest releases
-                        var response = await client.GetAsync("releases");
+            using var githubClient = new GitHubClient(options.Repository, options.Channel.Value, options.Username, options.Token);
+            var remoteVersionInfo = await githubClient.FetchRemoteVersionAsync();
 
-                        // Parse all the releases and versions
-                        var json = await response.Content.ReadAsStringAsync();
-                        var releases = JArray.Parse(json)
-                            .ToDictionary(r => Version.Parse(r["tag_name"]!.ToString()), r => r);
-
-                        // If the channel is staging, get the latest version,
-                        // while if the channel is release, get the latest stable version
-                        var latest = options.Channel == BuildChannel.Staging
-                            ? releases.MaxBy(r => r.Key)
-                            : releases.Where(r => r.Key.Revision == -1).MaxBy(r => r.Key);
-                        
-                        remoteVersion = latest.Key;
-                        release = latest.Value;
-                        
-                        AnsiConsole.MarkupLine($"[green]Remote version: {remoteVersion}[/]");
-                    }
-                    catch (Exception ex)
-                    {
-                        ExitWithError(ex);
-                    }
-                });
+            AnsiConsole.MarkupLineInterpolated($"[green]Remote version: {remoteVersionInfo.Version}[/]");
             
-            Version? currentVersion = null;
-
-            await AnsiConsole.Status()
-                .StartAsync("[yellow]Reading the current version...[/]", async ctx => 
-                {
-                    try
-                    {
-                        // Check if version.txt exists
-                        if (!File.Exists("version.txt"))
-                        {
-                            return;
-                        }
-                        
-                        var content = await File.ReadAllLinesAsync("version.txt");
-                        currentVersion = Version.Parse(content.First());
-                        
-                        AnsiConsole.MarkupLine($"[green]Current version: {currentVersion}[/]");
-                    }
-                    catch (Exception ex)
-                    {
-                        ExitWithError(ex);
-                    }
-                });
+            // Get the current version
+            var currentVersion = await FileSystemHelper.GetLocalVersionAsync();
 
             if (currentVersion is null)
             {
@@ -139,7 +62,7 @@ namespace Updater
             }
             else
             {
-                if (remoteVersion > currentVersion)
+                if (remoteVersionInfo.Version > currentVersion)
                 {
                     AnsiConsole.MarkupLine("[yellow]Update available![/]");
                     
@@ -163,230 +86,21 @@ namespace Updater
                 }
             }
             
-            // Check if OpenBullet2 is running
-            if (Process.GetProcessesByName("OpenBullet2.Web").Length > 0)
-            {
-                ExitWithError("OpenBullet 2 is currently running, please close it before updating!");
-                return;
-            }
-            
-            // Download the OpenBullet2.Web.zip file
-            MemoryStream stream = null!;
-            try
-            {
-                var build = release["assets"]!.First(t => t["name"]!.ToObject<string>()! == "OpenBullet2.Web.zip");
-                var size = build["size"]!.ToObject<double>();
-                var megaBytes = size / (1 * 1000 * 1000);
-                AnsiConsole.MarkupLine($"[yellow]Downloading the updated build ({megaBytes:0.00} MB)...[/]");
-                
-                var downloadUrl = build["url"]!.ToString();
-                await AnsiConsole.Progress()
-                    .Columns([
-                        new TaskDescriptionColumn(),
-                        new ProgressBarColumn(),
-                        new PercentageColumn()
-                    ])
-                    .StartAsync(async ctx => 
-                    {
-                        var downloadTask = ctx.AddTask("[green]Downloading[/]");
+            // Download the new build
+            await using var buildStream = await githubClient.DownloadBuildAsync(remoteVersionInfo);
+            AnsiConsole.MarkupLine("[green]Download complete![/]");
 
-                        var progress = new Progress<double>(p =>
-                        {
-                            downloadTask.Value = p;
-                        });
+            // Clean up the installation folder
+            await FileSystemHelper.CleanupInstallationFolderAsync();
 
-                        stream = await DownloadAsync(client, downloadUrl, progress); 
-                    });
-                
-                AnsiConsole.MarkupLine("[green]Download complete![/]");
-            }
-            catch (Exception ex)
-            {
-                ExitWithError(ex);
-            }
-            
-            AnsiConsole.MarkupLine("[yellow]Cleaning up the OB2 folder...[/]");
-            
-            var whitelist = new[]
-            {
-                "appsettings.json",
-                "Updater.exe",
-                "Updater.dll",
-                "UserData"
-            };
-
-            // The build-files.txt file contains a list of all the files in the current build.
-            // We will delete all those files and folders and clean up the directory.
-            if (File.Exists("build-files.txt"))
-            {
-                var entries = await File.ReadAllLinesAsync("build-files.txt");
-                
-                AnsiConsole.Status()
-                    .Start("[yellow]Deleting...[/]", ctx =>
-                    {
-                        foreach (var entry in entries.Where(e => !string.IsNullOrWhiteSpace(e)))
-                        {
-                            ctx.Status($"Deleting {entry}...");
-                    
-                            // If the entry is whitelisted, disregard it
-                            if (whitelist.Contains(entry))
-                            {
-                                continue;
-                            }
-                            
-                            var path = Path.Combine(Directory.GetCurrentDirectory(), entry);
-                            
-                            if (File.Exists(path))
-                            {
-                                File.Delete(path);
-                            }
-                            else if (Directory.Exists(path))
-                            {
-                                Directory.Delete(path, true);
-                            }
-                        }
-                    });
-                    
-            }
-            // If the file does not exist, we will delete everything except the whitelisted files and folders.
-            else
-            {
-                AnsiConsole.MarkupLine(
-                    "[yellow]build-files.txt not found, cleaning up everything not in the whitelist...[/]");
-                
-                AnsiConsole.Status()
-                    .Start("[yellow]Deleting...[/]", ctx =>
-                    {
-                        foreach (var entry in Directory.EnumerateFileSystemEntries(Directory.GetCurrentDirectory()))
-                        {
-                            var isDirectory = (File.GetAttributes(entry) & FileAttributes.Directory) == FileAttributes.Directory;
-                        
-                            // If the entry is whitelisted, disregard it
-                            if (whitelist.Contains(Path.GetFileName(entry)))
-                            {
-                                continue;
-                            }
-                        
-                            ctx.Status($"Deleting {entry}...");
-                    
-                            if (isDirectory)
-                            {
-                                Directory.Delete(entry, true);
-                            }
-                            else
-                            {
-                                File.Delete(entry);
-                            }
-                        }
-                    });
-            }
-
-            AnsiConsole.MarkupLine("[yellow]Extracting the archive...[/]");
-            
-            await AnsiConsole.Status()
-                .StartAsync("[yellow]Extracting...[/]", async ctx => 
-                {
-                    using var archive = new ZipArchive(stream);
-                    foreach (var entry in archive.Entries)
-                    {
-                        // Do not extract Updater.exe or Updater.dll
-                        if (entry.FullName.EndsWith("Updater.exe") || entry.FullName.EndsWith("Updater.dll"))
-                        {
-                            continue;
-                        }
-                        
-                        // Do not extract appsettings.json
-                        if (entry.FullName.EndsWith("appsettings.json"))
-                        {
-                            continue;
-                        }
-                
-                        // Do not extract UserData folder
-                        if (entry.FullName.StartsWith("UserData"))
-                        {
-                            continue;
-                        }
-                        
-                        // If the entry is a directory, disregard it
-                        if (entry.FullName.EndsWith('/'))
-                        {
-                            continue;
-                        }
-                        
-                        ctx.Status($"Extracting {entry.FullName}...");
-                
-                        var path = Path.Combine(Directory.GetCurrentDirectory(), entry.FullName);
-                        var dir = Path.GetDirectoryName(path);
-                        if (!Directory.Exists(dir))
-                        {
-                            Directory.CreateDirectory(dir!);
-                        }
-
-                        await using var fileStream = new FileStream(path, FileMode.Create);
-                        await using var entryStream = entry.Open();
-                        await entryStream.CopyToAsync(fileStream);
-                    }
-            
-                    await stream.DisposeAsync();
-                });
+            // Extract the archive
+            await FileSystemHelper.ExtractArchiveAsync(buildStream);
             
             AnsiConsole.MarkupLine("[green]The update was completed successfully. " +
                                    "You may now restart your OpenBullet 2 instance![/]");
             AnsiConsole.MarkupLine("[green]Press any key to exit...[/]");
             Console.ReadKey();
             Environment.Exit(0);
-        }
-
-        private static void ExitWithError(string message)
-        {
-            AnsiConsole.MarkupLine($"[red]Failed! {message}[/]");
-            AnsiConsole.MarkupLine("[red]Press any key to exit...[/]");
-            Console.ReadKey();
-            Environment.Exit(1);
-        }
-        
-        private static void ExitWithError(Exception ex)
-            => ExitWithError(ex.Message);
-
-        private static async Task<MemoryStream> DownloadAsync(HttpClient client, string url,
-            IProgress<double> progress)
-        {
-            using var request = new HttpRequestMessage(HttpMethod.Get, url);
-            
-            // We need to specify the Accept header as application/octet-stream
-            // to get the raw file instead of the json response
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
-            
-            using var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-            response.EnsureSuccessStatusCode();
-
-            var content = response.Content;
-            var total = response.Content.Headers.ContentLength!;
-            var downloaded = 0.0;
-
-            var memoryStream = new MemoryStream();
-            await using var stream = await content.ReadAsStreamAsync();
-
-            var buffer = new byte[81920];
-            var isMoreToRead = true;
-
-            do
-            {
-                var read = await stream.ReadAsync(buffer);
-                if (read == 0)
-                {
-                    isMoreToRead = false;
-                }
-                else
-                {
-                    await memoryStream.WriteAsync(buffer.AsMemory(0, read));
-                    downloaded += read;
-                    progress.Report(downloaded / total.Value * 100);
-                }
-            } while (isMoreToRead);
-            
-            memoryStream.Seek(0, SeekOrigin.Begin);
-            return memoryStream;
         }
     }
 }
