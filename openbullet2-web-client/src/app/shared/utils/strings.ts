15f57a01b54381c6e47e750e62ace10c106541ce
Ruri <ruridev@protonmail.com>
2024-07-07 02:12:34 +0200

    Fixed generateApiKey in sharing.component.ts and added generateUuidv4
    Closes #1034

--- openbullet2-web-client/src/app/shared/utils/strings.ts
+++ openbullet2-web-client/src/app/shared/utils/strings.ts
@@ -1,4 +1,4 @@
-function randomString(length: number) {
+export function randomString(length: number) {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
@@ -8,4 +8,11 @@ function randomString(length: number) {
     counter += 1;
   }
   return result;
+}
+
+export function generateUuidv4() {
+  const str = () => (`00000000000000000${(Math.random() * 0xffffffffffffffff).toString(16)}`).slice(-16);
+  const a = str();
+  const b = str();
+  return `${a.slice(0, 8)}-${a.slice(8, 12)}-4${a.slice(13)}-${b.slice(0, 4)}-${b.slice(4)}`;
 }
